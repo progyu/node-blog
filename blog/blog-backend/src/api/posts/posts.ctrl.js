3085bb07bc99f1b432d9ba334668623d909b0735
progyu <rocket7101@gmail.com>
2019-12-15 16:04:44 +0900

    feat: Verify ObjectId

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,6 +1,18 @@
 import Post from '../../models/post';
+import mongoose from 'mongoose';
 
-/*
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; // Bad Request
+    return;
+  }
+  return next();
+};
+
+/*`
   POST /api/posts
   {
     title: '제목',
@@ -74,18 +86,18 @@ export const remove = async ctx => {
   }
 */
 export const update = async ctx => {
- const { id } = ctx.params;
- try {
-  const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
-    new: true, // 이 값을 설정하면 업데이트된 데이터를 반환
-    // false일 때는 업데이트 되기 전의 데이터를 반환
-  }).exec();
-  if (!post) {
-    ctx.status = 404;
-    return;
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환
+      // false일 때는 업데이트 되기 전의 데이터를 반환
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  ctx.body = post;
- } catch (e) {
-   ctx.throw(500, e);
- }
 };
