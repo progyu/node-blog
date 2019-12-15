04c6d099da9a2fe5d0c605f2877efba726e71403
progyu <rocket7101@gmail.com>
2019-12-16 00:49:39 +0900

    feat: Make register api
    Verify Request Body
    Double check username
    Hash a password by bcrypt library
    Make User schema and model
    Make auth router
    Make static methods and instance methods

---
+++ blog/blog-backend/src/models/user.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+UserSchema.methods.setPassword = async function(password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function(password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result; /// true / false
+};
+
+UserSchema.methods.serialize = function() {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
+}
+
+UserSchema.statics.findByUsername = function(username) {
+  return this.findOne({ username });
+}
+
+const User = mongoose.model('User', UserSchema);
+export default User;
