a05bd2664c327a9ea773c2295c889a60b66cf535
progyu <rocket7101@gmail.com>
2019-12-15 15:53:32 +0900

    feat: Make update api
    HTTP method patch. Find specific id

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -8,6 +8,6 @@ posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
 posts.get('/:id', postsCtrl.read);
 posts.delete('/:id', postsCtrl.remove);
-// posts.patch('/:id', postsCtrl.update);
+posts.patch('/:id', postsCtrl.update);
 
 export default posts;
--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -65,4 +65,27 @@ export const remove = async ctx => {
   }
 };
 
-export const update = ctx => {};
+/*
+  PATCH /api/posts/:id
+  {
+    title: '수정',
+    body: '수정 내용',
+    tags: ['수정','태그']
+  }
+*/
+export const update = async ctx => {
+ const { id } = ctx.params;
+ try {
+  const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+    new: true, // 이 값을 설정하면 업데이트된 데이터를 반환
+    // false일 때는 업데이트 되기 전의 데이터를 반환
+  }).exec();
+  if (!post) {
+    ctx.status = 404;
+    return;
+  }
+  ctx.body = post;
+ } catch (e) {
+   ctx.throw(500, e);
+ }
+};
