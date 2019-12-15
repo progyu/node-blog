d88b5b6ed92dfb67bd1666f16c40982cb45d81d3
progyu <rocket7101@gmail.com>
2019-12-15 15:41:56 +0900

    feat: Make remove api
    HTTP method delete. find specific id

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -52,5 +52,17 @@ export const read = async ctx => {
   }
 };
 
-export const remove = ctx => {};
+/*
+  Delete /api/posts:id
+*/
+export const remove = async ctx => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content ( 성공하기는 했지만 응답할 데이터는 없음)
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
 export const update = ctx => {};
