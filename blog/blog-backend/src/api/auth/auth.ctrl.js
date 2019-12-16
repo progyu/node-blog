f2d41fd8d29ebb56cf6038649fc170ca737082af
progyu <rocket7101@gmail.com>
2019-12-16 12:49:19 +0900

    feat: Make login api
    Verify username, password

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -19,7 +19,7 @@ export const register = async ctx => {
     password: Joi.string().required(),
   });
   const result = Joi.validate(ctx.request.body, schema);
-  if(result.error) {
+  if (result.error) {
     ctx.status = 400;
     ctx.body = result.error;
     return;
@@ -29,7 +29,7 @@ export const register = async ctx => {
   try {
     // username이 이미 존재하는지 확인
     const exists = await User.findByUsername(username);
-    if(exists) {
+    if (exists) {
       ctx.status = 409; // Conflict
       return;
     }
@@ -47,8 +47,39 @@ export const register = async ctx => {
   }
 };
 
+/*
+  POST /api/auth/login
+  {
+    username: 'lee',
+    password: 'mypass123'
+  }
+*/
 export const login = async ctx => {
-  // 로그인
+  const { username, password } = ctx.request.body;
+
+  // username, password가 없으면 에러 처리
+  if (!username || !password) {
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+    // 계정이 존재하지 않으면 에러 처리
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password);
+    // 잘못된 비밀번호
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const check = async ctx => {
