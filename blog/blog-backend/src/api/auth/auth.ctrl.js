f17cd309d29e54faf2c1b7460240d8dc20a7f12b
progyu <rocket7101@gmail.com>
2019-12-16 13:04:36 +0900

    feat: Issue a JWT token
    Set token at cookie
    Vaild for 7 days

--- blog/blog-backend/src/api/auth/auth.ctrl.js
+++ blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -42,6 +42,12 @@ export const register = async ctx => {
 
     // 응답할 데이터에서 hashedPassword 필드 제거
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -77,6 +83,12 @@ export const login = async ctx => {
       return;
     }
     ctx.body = user.serialize();
+    const token = user.generateToken();
+    
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
