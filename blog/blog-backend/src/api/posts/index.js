fa295b2c81def906b3e431671a797cfe8f160f54
progyu <rocket7101@gmail.com>
2019-12-16 14:13:27 +0900

    feat: Post only when logged in
    Put user info when creating a post
    Use checkLoggedIn middleware

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -1,13 +1,14 @@
 import Router from 'koa-router';
-
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
+
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/',checkLoggedIn, postsCtrl.write);
 posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+posts.delete('/:id',checkLoggedIn, postsCtrl.checkObjectId, postsCtrl.remove);
+posts.patch('/:id',checkLoggedIn, postsCtrl.checkObjectId, postsCtrl.update);
 
 export default posts;
--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -44,6 +44,7 @@ export const write = async ctx => {
     title,
     body,
     tags,
+    user: ctx.state.user,
   });
   try {
     await post.save();
