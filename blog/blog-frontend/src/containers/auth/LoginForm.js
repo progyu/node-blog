a620f2c9b5c43a6a5e465977756b2b4529aeb92c
progyu <rocket7101@gmail.com>
2019-12-19 17:46:48 +0900

    feat: Handle login errors
    Display an error on the screen

--- blog/blog-frontend/src/containers/auth/LoginForm.js
+++ blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { changeField, initializeForm, login } from '../../modules/auth';
@@ -6,6 +6,8 @@ import AuthForm from '../../components/auth/AuthForm';
 import { check } from '../../modules/user';
 
 const LoginForm = ({ history }) => {
+  const [error, setError] =useState(null);
+
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
@@ -41,6 +43,7 @@ const LoginForm = ({ history }) => {
     if (authError) {
       console.log('오류 발생');
       console.log(authError);
+      setError('로그인 실패');
       return;
     }
     if (auth) {
@@ -61,6 +64,7 @@ const LoginForm = ({ history }) => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
