7c3cc324beb13da977a2a3bef1e6fa2aba4a8a1f
progyu <rocket7101@gmail.com>
2019-12-24 14:27:08 +0900

    feat: Move to login page click login button
    feat: Move to home page click logo
    add: Create head component

--- blog/blog-frontend/src/components/common/Button.js
+++ blog/blog-frontend/src/components/common/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
 
-const StyledButton = styled.button`
+const buttonStyle = css`
   border: none;
   border-radius: 4px;
   font-size: 1rem;
@@ -24,7 +25,7 @@ const StyledButton = styled.button`
       padding-bottom: 0.75rem;
       width: 100%;
       font-size: 1.125rem;
-  `}
+    `}
 
   ${props =>
     props.cyan &&
@@ -36,6 +37,20 @@ const StyledButton = styled.button`
     `}
 `;
 
-const Button = props => <StyledButton {...props} />
+const StyledButton = styled.button`
+  ${buttonStyle}
+`;
+
+const StyledLink = styled(Link)`
+  ${buttonStyle}
+`;
+
+const Button = props => {
+  return props.to ? (
+    <StyledLink {...props} cyan={props.cyan ? 1 : 0} />
+  ) : (
+    <StyledButton {...props} />
+  );
+};
 
 export default Button;
